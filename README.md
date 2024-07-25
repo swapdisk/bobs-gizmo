b87e18fdff62926bf5046ad8165d767ebded57c0
Bob Mader <bmader@redhat.com>
2024-07-25 14:27:03 -0500

    add falcon sensor driver

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ What we have here is an M5Stack [Fire](https://docs.m5stack.com/en/core/fire) de
 
 The app presents a slide show that advances to the next slide image each time the `A` button is pressed. Pressing the `C` button displays an "easter egg" image showing a kernel panic. Of course, everybody knows this thing isn't running Linux, so this was good for some laughs! 
 
+Updated July 2024: The easter egg function has been enhanced in celebration of CrowdStrike Global IT Outage Day. Check out the last photo below.
+
 Pro tip: the png files were created with a transparent background by exporting from svg files in Inkscape. This significantly increased the rendering speed on the screen.
 
 I used a hotel room key and a scrap of Lego block to attach the device to my conference badge. Here are some photos of the final project.
@@ -19,5 +21,7 @@ I used a hotel room key and a scrap of Lego block to attach the device to my con
 ![Value proposition](./images/value_photo.jpg)
 
 ![Kernel panic easter egg](./images/panic_photo.jpg)
+
+![Windows blue screen of death easter egg](./images/bsod_photo.jpg)
 
 Enjoy with beverage of choice!
--- butext.py
+++ butext.py
@@ -7,21 +7,23 @@ setScreenColor(0x000000)
 
 i = None
 c = None
+p = None
 image0 = M5Img(0, 0, "res/default.jpg", False)
 label0 = M5TextBox(0, 0, "label0", lcd.FONT_Default, 0xFFFFFF, rotate=0)
 
 # Display battery status
 def battstat():
-  global i, c
+  global i, c, p
   if power.isCharging():
     c = '(charging)'
   else:
     c = ''
+  label0.setText('')
   label0.setText(str((str((power.getBatteryLevel())) + str(((str('% ') + str(c)))))))
 
 # Next slide callback
 def buttonA_wasPressed():
-  global i, c
+  global i, c, p
   i = (i if isinstance(i, Number) else 0) + 1
   setScreenColor(0x000000)
   if i==1:
@@ -38,7 +40,7 @@ btnA.wasPressed(buttonA_wasPressed)
 
 # Battery status callback
 def buttonB_wasReleased():
-  global i, c
+  global i, c, p
   speaker.tone(2200, 100)
   battstat()
   pass
@@ -46,13 +48,20 @@ btnB.wasReleased(buttonB_wasReleased)
 
 # Kernel panic callback  
 def buttonC_wasPressed():
-  global i, c
-  setScreenColor(0x000000)
-  image0.changeImg("res/kpanic2.png")
+  global i, c, p
+  p = (p if isinstance(p, Number) else 0) + 1
+  if p==1:
+    setScreenColor(0x000000)
+    image0.changeImg("res/kpanic2.png")
+  else:
+    setScreenColor(0x2067b2)
+    image0.changeImg("res/bsod.png")
+    p = 0
   pass
 btnC.wasPressed(buttonC_wasPressed)
 
 # Main
 i = 0
+p = 0
 battstat()
 image0.changeImg("res/rhblogo.png")
